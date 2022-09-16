180247ed760bd2569e344a2f0610bd298e74f00b
Kunyu He <kunyu.he.ds@gmail.com>
2022-09-16 22:41:05 +0800

    FIX: callbacks not working

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ Created: 9/11/2022 10:46 PM
 """
 import dash
 import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dateutil.relativedelta import relativedelta
 
 from data.shareholding import ShareHoldingDAO
-from static import APP_NAME
+from static import APP_NAME, CONTENT_STYLE, SIDEBAR_STYLE
+from utils import today
 
 dao = ShareHoldingDAO()
 
@@ -18,3 +21,44 @@ app = dash.Dash(
 )
 app.title = APP_NAME
 server = app.server
+app.layout = html.Div(
+    [
+        # Left column
+        html.Div(
+            children=[
+                html.P("Input Stock Code:"),
+                dcc.Input(
+                    id='stockcode',
+                    type="text",
+                ),
+                html.Br(),
+                html.Br(),
+                html.P(children="Select Date Range:"),
+                dcc.DatePickerRange(
+                    id='daterange',
+                    min_date_allowed=today() + relativedelta(years=-1),
+                    max_date_allowed=today() + relativedelta(days=-1),
+                    start_date=today() + relativedelta(days=-7),
+                    end_date=today() + relativedelta(days=-1)
+                )
+            ],
+            style=SIDEBAR_STYLE
+        ),
+        html.Div(
+            [
+                dbc.CardHeader(
+                    dbc.Tabs(
+                        [
+                            dbc.Tab(label="Trend", tab_id="trend"),
+                            dbc.Tab(label="Transactions", tab_id="transactions")
+                        ],
+                        id="tab",
+                        active_tab="trend"
+                    )
+                ),
+                dbc.CardBody(html.Div(id="card-content", className="card-text")),
+            ],
+            style=CONTENT_STYLE
+        )
+    ]
+)
--- index.py
+++ index.py
@@ -3,57 +3,10 @@ Name:    index.py
 Author:  kyh
 Created: 9/16/2022 10:28 PM
 """
-import dash_bootstrap_components as dbc
-from dash import dcc, html
 from dash.dependencies import Input, Output
-from dateutil.relativedelta import relativedelta
 
 import pages
 from app import app
-from static import CONTENT_STYLE, SIDEBAR_STYLE
-from utils import today
-
-app.layout = html.Div(
-    [
-        # Left column
-        html.Div(
-            children=[
-                html.P("Input Stock Code:"),
-                dcc.Input(
-                    id='stockcode',
-                    type="text",
-                ),
-                html.Br(),
-                html.Br(),
-                html.P(children="Select Date Range:"),
-                dcc.DatePickerRange(
-                    id='daterange',
-                    min_date_allowed=today() + relativedelta(years=-1),
-                    max_date_allowed=today() + relativedelta(days=-1),
-                    start_date=today() + relativedelta(days=-7),
-                    end_date=today() + relativedelta(days=-1)
-                )
-            ],
-            style=SIDEBAR_STYLE
-        ),
-        html.Div(
-            [
-                dbc.CardHeader(
-                    dbc.Tabs(
-                        [
-                            dbc.Tab(label="Trend", tab_id="trend"),
-                            dbc.Tab(label="Transactions", tab_id="transactions")
-                        ],
-                        id="tab",
-                        active_tab="trend"
-                    )
-                ),
-                dbc.CardBody(html.Div(id="card-content", className="card-text")),
-            ],
-            style=CONTENT_STYLE
-        )
-    ]
-)
 
 
 @app.callback(Output("card-content", "children"), [Input("tab", "active_tab")])
